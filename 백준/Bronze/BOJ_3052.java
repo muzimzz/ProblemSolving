4c88d47cbf6b50f5ea651b5ca9a8250e5dcf2a66
muzimzz <rlagusdn0914@naver.com>
2026-04-15 10:51:15 +0900

    backup

---
+++ 백준/Bronze/BOJ_3052.java
@@ -0,0 +1,23 @@
+package Bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_3052 {
+
+    static int SIZE = 10;
+    static int NUM = 42;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < SIZE; i++) {
+            int n = Integer.parseInt(br.readLine());
+            set.add(n % NUM);
+        }
+
+        System.out.println(set.size());
+    }
+}
