c7a15c9d474bb74e11970f9ce7ea0c1f55e3df63
muzimzz <rlagusdn0914@naver.com>
2026-04-16 15:59:29 +0900

    두 개 뽑아서 더하기

--- 프로그래머스/Lv1/P_68644.java
+++ 프로그래머스/Lv1/P_68644.java
@@ -1,4 +1,27 @@
 package Lv1;
 
-public class P_68644 {
+import java.util.*;
+import java.io.*;
+
+class P_68644 {
+    public int[] solution(int[] numbers) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                if (i == j) continue;
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        list.sort(null);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
 }
